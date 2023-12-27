ea246b64cd38867e7c87eb482f58986256b98735
jwshin <singee98@naver.com>
2023-12-27 11:31:51 +0900

    java

---
+++ HelloWorld/src/HelloWorld/dataType.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class dataType {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		int a = 263;
+		System.out.println(a);
+		
+		byte b = (byte)a;
+		System.out.println(b);
+	}
+
+}
---
+++ HelloWorld/src/HelloWorld/java_practice.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class java_practice {
+
+	public static void main(String[] args) {
+		boolean a = true;
+		 boolean b = false;
+		 boolean c = !b; // b의 값을 반대로 바꾸어 대입합니다.
+		 System.out.println(a); // true가 출력됩니다.
+		 System.out.println(!a); // 값을 바꾸어 출력합니다.
+		 System.out.println(b); // false가 출력됩니다.
+		 System.out.println(c); // true가 출력됩니다.
+	}
+	}
