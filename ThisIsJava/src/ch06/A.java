2a3b347e0e9f7b2cda4d4e254271f8df035471d3
jwshin <singee98@naver.com>
2024-01-04 15:44:59 +0900

    23.01.04

--- ThisIsJava/src/ch06/A.java
+++ ThisIsJava/src/ch06/A.java
@@ -1,7 +1,34 @@
 package ch06;
 
 public class A {
-	A a1 = new A(true);
+	
+	public int field1;
+	int field2;
+	private int field3;
+	
+	public A(){
+		
+		field1 = 1;
+		field2 = 1;
+		field3 = 1;
+
+		method1();
+		method2();
+		method3();
+	}
+	
+	public void method1() {
+		
+	}
+	public void method2() {
+		
+	}
+	public void method3() {
+		
+	}
+	
+	
+	/*A a1 = new A(true);
 	A a2 = new A(1);
 	A a3 = new A("문자열");
 	
@@ -15,6 +42,6 @@ public class A {
 	
 	private A(String s) {
 		
-	}
+	}*/
 	
 }
